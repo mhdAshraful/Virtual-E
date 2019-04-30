fb78cc83c537c7f0c6448e5901de2821de2ffdef
Ashraf0011 <mycdu.edu@icloud.com>
2019-04-30 14:20:38 +0930

    'Hault for D2' VR stream needds to be added, Completed: Icon names removed for compatibility issue, Grades and SubmitHere page replaced with AssesmentsPage, Grades buttom removed from Tab bar

--- src/app/submit/submit.page.ts
+++ src/app/submit/submit.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-submit',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmitPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  navigateToUnits() {
+    this.router.navigate(['units']);
   }
 
 }
