bfbadc2a63a47d0312307e12f738d4562e94f2cb
Ashraf0011 <mycdu.edu@icloud.com>
2019-05-08 12:39:54 +0930

    card positioning

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,  // virtual-e
   firebase: {
-        apiKey: "AIzaSyAtwq4klJIpsBKVDmUnGmsYDm7sTPZU4So",
-        authDomain: "smartuni-virtual-e.firebaseapp.com",
-        databaseURL: "https://smartuni-virtual-e.firebaseio.com",
-        projectId: "smartuni-virtual-e",
-        storageBucket: "smartuni-virtual-e.appspot.com",
-        messagingSenderId: "1017993026221",
-        appId: "1:1017993026221:web:d9ccd0e86145b179"
+
   }
 };
 
