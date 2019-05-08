1f61ebadbaafd38b7db63f512cc3e1616c8c7fee
Ashraf0011 <mycdu.edu@icloud.com>
2019-05-08 12:48:13 +0930

    vrview page added

---
+++ src/app/vrview/vrview.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vrview',
+  templateUrl: './vrview.page.html',
+  styleUrls: ['./vrview.page.scss'],
+})
+export class VrviewPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
